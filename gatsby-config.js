2ac41d4e3474c8234253a52bfedc046346ed4281
Naren Mudivarthy <narenmudivarthy@Narens-MacBook-Air.local>
2024-08-19 07:25:34 +0000

    19, ఆగ 2024 12:25 AM

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+// gatsby-config.js
+module.exports = {
+    plugins: [
+      {
+        resolve: `gatsby-theme-garden`,
+        options: {
+          // basePath defaults to `/`
+          basePath: `/literature-notes`,
+          rootNote: `/literature-notes/notes`,
+          contentPath: `/notes`,
+        },
+      },
+    ],
+  };
